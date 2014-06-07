e986917d7584a05506ca6adcdf5301f55df2da0c
John Frizelle <john.frizelle@feedhenry.com>
2014-06-07 23:33:51 +0100

    #7364 - Don't reference fh-mbaas-express directly, get it via fh-mbaas-api.

--- cloud/application.js
+++ cloud/application.js
@@ -1,18 +1,24 @@
-//the main file for the cloud app
-var webapp = require('fh-webapp');
 var express = require('express');
-$fh = require('fh-api');
-var mainjs = require('main.js');
+var mbaasApi = require('fh-mbaas-api');
+var mbaasExpress = mbaasApi.mbaasExpress();
+var mainjs = require('./main.js');
 
 var app = express();
-app.use('/sys', webapp.sys(mainjs));
-app.use('/mbaas', webapp.mbaas);
-app.use('/cloud', webapp.cloud(mainjs));
+app.use('/sys', mbaasExpress.sys(mainjs));
+app.use('/mbaas', mbaasExpress.mbaas);
+
+// Note: important that this is added just before your own Routes
+app.use(mbaasExpress.fhmiddleware());
+
+app.use('/cloud', mbaasExpress.cloud(mainjs));
 
 // You can define custom URL handlers here, like this one:
 app.use('/', function(req, res){
   res.end('Your Cloud App is Running');
 });
+
+// Important that this is last!
+app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001;
 module.exports = app.listen(port, function(){
